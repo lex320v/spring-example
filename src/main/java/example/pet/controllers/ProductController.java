02c2c90fa403174b35f0b476a9280294190640d3
alexandr <usachev.alexander@fusion-tech.pro>
2023-12-28 01:17:51 +0300

    initial commit

---
+++ src/main/java/example/pet/controllers/ProductController.java
@@ -0,0 +1,44 @@
+package example.pet.controllers;
+
+import example.pet.models.Product;
+import example.pet.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// requiredArgsConstructor
+@Controller
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "product-info";
+    }
+
+    @PostMapping("product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
+    }
+}
